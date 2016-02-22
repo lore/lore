2acbf7de1941c8c0c08af2214cabc6e4a30ca6ee
Jason Hansen <jason_hansen@outlook.com>
2016-02-21 23:11:32 -0700

    replace monorepo library with Makefiles
    finish replacing monorepo with Makefiles
    
    fix initializers test
    
    remove duplicate files
    
    replace monorepo with Makefiles
    
    finish replacing monorepo with Makefiles

---
+++ lore/packages/lore-core/test/integration/initializers.spec.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+var Lore = require('../../src/app/index');
+var loaderHelper = require('../helpers/loaderHelper');
+
+describe('lore#initializers', function() {
+  var lore = null;
+  var ran = false;
+
+  beforeEach(function() {
+    loaderHelper.stub({
+      initializers: [
+        function() {
+          ran = true;
+        }
+      ]
+    });
+    lore = new Lore();
+  });
+
+  it('it should run any loaders', function () {
+    lore.build();
+    expect(ran).to.equal(true);
+  });
+
+});
+
