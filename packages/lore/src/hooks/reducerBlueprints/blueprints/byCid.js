c791c555394e854df79db23dc5d5c69884adac90
Jason Hansen <jason_hansen@outlook.com>
2016-03-09 04:50:10 -0700

    [lore] overwrite incorrect String id with correct Number id

--- packages/lore/src/hooks/reducerBlueprints/blueprints/byCid.js
+++ packages/lore/src/hooks/reducerBlueprints/blueprints/byCid.js
@@ -5,13 +5,30 @@ var _ = require('lodash');
  * byCid Reducer Blueprint
  */
 
+// IMPORTANT!! This is a modified version of _.findKey that doesn't compare types.
+// This is needed because when the API server uses Numbers as primary keys, the
+// initial GET request based on an ID in the url is created as a string in the
+// primary key ({id: '123'}). But when the response comes back from the server, the
+// id is corrected in the data to be a number ({id: 123}).  Without this modified
+// function, you can end up polluting the reducer store with duplicate data because it
+// will see an id of '123' and an id of 123 as two different objects. This function
+// will merge them together, and overwrite the initially incorrect '123' id with the
+// proper 123 id.
+function findKey(state, object) { // 1 or '1'
+  var keys = _.keys(state); // [1,2,'3']
+
+  return _.find(keys, function(key) {
+    return state[key].id == object.id;
+  });
+}
+
 function addOrUpdateByCid(nextState, payload) {
   var cid = payload && payload.cid;
   var cidWithDuplicatedId = null;
 
   if (cid) {
     if (payload.id) {
-      cidWithDuplicatedId = _.findKey(nextState, { id: payload.id });
+      cidWithDuplicatedId = findKey(nextState, { id: payload.id });
 
       // if we find a cid with a matching id, update that model with
       // the latest data, but keep the original cid
