c4e479b9fb3794435e21a8d7d55cb8c8591ba63d
Jason Hansen <jason_hansen@outlook.com>
2016-07-19 21:56:11 -0700

    [examples] add infinite scrolling example

---
+++ examples/infinite-scrolling/src/components/LoadMoreButton.js
@@ -0,0 +1,48 @@
+var React = require('react');
+var PayloadStates = require('../constants/PayloadStates');
+
+module.exports = React.createClass({
+  displayName: 'LoadMoreButton',
+
+  propTypes: {
+    lastPage: React.PropTypes.object.isRequired,
+    onLoadMore: React.PropTypes.func.isRequired
+  },
+
+  getStyles: function() {
+    return {
+      footer: {
+        textAlign: 'center',
+        marginTop: '32px',
+        marginBottom: '64px'
+      },
+      button: {
+        outline: 'none'
+      }
+    }
+  },
+
+  render: function() {
+    var styles = this.getStyles();
+    var lastPage = this.props.lastPage;
+
+    if(lastPage.state === PayloadStates.FETCHING) {
+      return (
+        <div style={styles.footer}>
+          <button className="btn btn-default btn-lg disabled" style={styles.button}>
+            Loading...
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div style={styles.footer}>
+        <button className="btn btn-default btn-lg" style={styles.button} onClick={this.props.onLoadMore}>
+          Load More
+        </button>
+      </div>
+    );
+  }
+
+});
