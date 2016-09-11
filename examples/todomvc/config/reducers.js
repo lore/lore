52c8a7feb2e0bcd6c4cc74f8db69d1bfa3447ccf
Jason Hansen <jason_hansen@outlook.com>
2016-09-11 01:31:39 -0700

    [lore] prepare v0.9.0 release

--- examples/todomvc/config/reducers.js
+++ examples/todomvc/config/reducers.js
@@ -1,14 +1,32 @@
 /**
  * Configuration file for reducers
- * TODO: figure out what options go here...
+ *
+ * This file is where you define overrides for the default reducer behaviors.
  */
 
 module.exports = {
 
   /****************************************************************************
-  *                                                                           *
-  * No options currently exist                                                *
-  *                                                                           *
-  ****************************************************************************/
+   *                                                                           *
+   * Specify dependencies between child reducers, which will control the order *
+   * they are called in as well as what data is passed in through the third    *
+   * options argument:                                                         *
+   * function someReducer(state, action, options) {
+   *   // `options.nextState` will contain the results of the child
+   *   // reducers you have declared a dependency on
+   * }
+   *                                                                           *
+   ****************************************************************************/
+
+  // This is the dependency structure for models by default,
+  // using the built-in blueprints:
+
+  // dependencies: {
+  //   modelName: {
+  //     byId: [],
+  //     byCid: [],
+  //     find: ['byId', 'byCid']
+  //   }
+  // }
 
 };
--- packages/lore-generate-new/templates/es6/config/reducers.js
+++ packages/lore-generate-new/templates/es6/config/reducers.js
@@ -2,15 +2,31 @@
  * Configuration file for reducers
  *
  * This file is where you define overrides for the default reducer behaviors.
- * TODO: figure out what options go here...
  */
 
 export default {
 
   /****************************************************************************
-  *                                                                           *
-  * No options currently exist                                                *
-  *                                                                           *
-  ****************************************************************************/
+   *                                                                           *
+   * Specify dependencies between child reducers, which will control the order *
+   * they are called in as well as what data is passed in through the third    *
+   * options argument:                                                         *
+   * function someReducer(state, action, options) {
+   *   // `options.nextState` will contain the results of the child
+   *   // reducers you have declared a dependency on
+   * }
+   *                                                                           *
+   ****************************************************************************/
 
-}
+  // This is the dependency structure for models by default,
+  // using the built-in blueprints:
+
+  // dependencies: {
+  //   modelName: {
+  //     byId: [],
+  //     byCid: [],
+  //     find: ['byId', 'byCid']
+  //   }
+  // }
+
+};
