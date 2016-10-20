63015ed850acd584d9a598278a50c670bd119412
Jason Hansen <jason_hansen@outlook.com>
2016-10-19 21:00:21 -0700

    [lore-utils] add payload and payloadCollection

--- packages/lore-utils/src/index.js
+++ packages/lore-utils/src/index.js
@@ -1,5 +1,29 @@
+function payload(model, state, error) {
+  return {
+    id: model.id,
+    cid: model.cid,
+    state: state,
+    error: error || {},
+    data: model.toJSON()
+  };
+}
+
+function payloadCollection(collection, state, error, query) {
+  return {
+    state: state,
+    error: error || {},
+    data: collection.models.map(function( model ) {
+      return payload(model, state, error);
+    }),
+    query: query,
+    meta: collection.meta
+  };
+}
+
 module.exports = {
   ActionTypes: require('./ActionTypes'),
   PayloadStates: require('./PayloadStates'),
-  Hook: require('./Hook')
+  Hook: require('./Hook'),
+  payload: payload,
+  payloadCollection: payloadCollection
 };
