9dbdba89518c3c99cfea03a42e6a9b6807167f74
Jason Hansen <jason_hansen@outlook.com>
2016-12-12 00:45:13 -0700

    [lore] remove dependency on react-tap-event-plugin

--- packages/lore/src/app/mount.js
+++ packages/lore/src/app/mount.js
@@ -1,7 +1,6 @@
 var _ = require('lodash');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var injectTapEventPlugin = require('react-tap-event-plugin');
 var getRoot = require('./getRoot');
 
 /**
@@ -13,12 +12,6 @@ var getRoot = require('./getRoot');
  * @param {Function} cb callback to call once the app is mounted to the DOM
  */
 module.exports = function mount(store, routes, history, cb) {
-  // Needed for onTouchTap
-  // Can go away when react 1.0 release
-  // Check this repo:
-  // https://github.com/zilverline/react-tap-event-plugin
-  injectTapEventPlugin();
-
   const Root = getRoot(store, routes);
 
   ReactDOM.render(React.createElement(Root, {
