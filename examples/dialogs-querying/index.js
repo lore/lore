49bc31c66bc7888b9692b738d5eea9def2404860
Jason Hansen <jason_hansen@outlook.com>
2017-01-25 21:44:48 -0700

    [examples] update dialogs-querying for v0.11-rc

--- examples/dialogs-querying/index.js
+++ examples/dialogs-querying/index.js
@@ -1,19 +1,33 @@
-var Lore = require('lore');
+/**
+ * This file kicks off the build process for the application.  It also attaches
+ * the Lore singleton to the window, so you can access it from the command line
+ * in case you need to play with it or want to manually kick off actions or check
+ * the reducer state (through `lore.actions.xyz`, `lore.reducers.xyz`,
+ * `lore.models.xyz`, etc.)
+ **/
+
+var lore = require('lore');
+
+// Needed for using onTouchTap and removing the 300ms touch delay from mobile devices
+var injectTapEventPlugin = require('react-tap-event-plugin');
+injectTapEventPlugin();
 
 // Allows you to access your lore app globally as well as from within
 // the console. Remove this line if you don't want to be able to do that.
-window.lore = Lore;
+window.lore = lore;
 
 // Summon the app!
-Lore.summon({
+lore.summon({
   hooks: {
-    actions: require("lore-hook-actions"),
-    bindActions: require("lore-hook-bind-actions"),
-    collections: require("lore-hook-collections"),
-    connect: require("lore-hook-connect"),
-    dialog: require("lore-hook-dialog"),
-    models: require("lore-hook-models"),
-    reducers: require("lore-hook-reducers"),
-    redux: require("lore-hook-redux")
+    actions: require('lore-hook-actions'),
+    bindActions: require('lore-hook-bind-actions'),
+    collections: require('lore-hook-collections'),
+    connections: require('lore-hook-connections'),
+    connect: require('lore-hook-connect'),
+    dialog: require('lore-hook-dialog'),
+    dialogs: require('lore-hook-dialogs-material-ui'),
+    models: require('lore-hook-models'),
+    reducers: require('lore-hook-reducers'),
+    redux: require('lore-hook-redux')
   }
 });
