9f1fb26a913e42fc184ab8abcc3ab741be1dba4a
Jason Hansen <jason_hansen@outlook.com>
2017-05-10 21:33:02 -0700

    [upgrade-guides] update ES6 and ESNext files

---
+++ upgrade-guides/v0.11-v0.12/esnext/config/env/production.js
@@ -0,0 +1,28 @@
+/**
+ * Production environment settings
+ *
+ * This file is where you define overrides for any of the config settings that
+ * should only be applied in the production environment.
+ *
+ * The production environment is defined as 'process.env.NODE_ENV=production' and
+ * is automatically set when webpack is invoked using the --env=production argument.
+ **/
+
+export default {
+
+  /**
+   * To override a config setting, specify the name of the config file you
+   * want to override, followed by the settings you want to change.
+   */
+
+  // actions: {
+  //   normalize: true
+  // },
+
+  // connections: {
+  //   default: {
+  //     apiRoot: 'https://api.example.dev'
+  //   }
+  // }
+
+}
