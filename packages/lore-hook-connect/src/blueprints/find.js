1b9b8dcab0ca3ccbe6046a4c78310511486dc383
Jason Hansen <jason_hansen@outlook.com>
2017-09-12 15:57:56 -0700

    [lore-hook-connect] add ability to include and exclude data in find blueprint

--- packages/lore-hook-connect/src/blueprints/find.js
+++ packages/lore-hook-connect/src/blueprints/find.js
@@ -1,4 +1,5 @@
 import toJsonKey from '../utils/toJsonKey';
+import _ from 'lodash';
 
 /**
  * Find Connection Blueprint
@@ -8,12 +9,62 @@ export default {
 
   defaults: {
     where: {},
-    pagination: {}
+    pagination: {},
+    include: {
+      where: function(model) {
+        return false;
+      },
+      uniqBy: function(model) {
+        return model.cid;
+      },
+      sortBy: function(model) {
+        return true;
+      }
+    },
+    exclude: {
+      where: function(model) {
+        return false;
+      }
+    }
   },
 
   getPayload: function(reducerState, params) {
     const jsonKey = toJsonKey(params);
-    return reducerState[jsonKey];
+    let state = reducerState[jsonKey];
+
+    if (state) {
+      let data = _
+        .chain(reducer.byCid)
+        .transform(function(result, model) {
+          result.push(model);
+        }, []);
+
+      if (params.include.where) {
+        data = data.filter(params.include.where);
+      }
+
+      data = data.value();
+
+      state = _.assign({}, state, {
+        data: state.data.concat(data)
+      });
+
+      if (params.include.uniqBy) {
+        state.data = _.uniqBy(state.data, params.include.uniqBy);
+      }
+
+      if (params.include.sortBy) {
+        state.data = _.sortBy(state.data, params.include.sortBy);
+      }
+
+      if (params.exclude.where) {
+        state.data = _.filter(state.data, function(model) {
+          return !params.exclude.where(model);
+        });
+      }
+    }
+
+    return state;
   },
 
   callAction: function(action, params) {
