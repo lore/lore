49e3f6587116b506702aab657a4a03316126d41a
Jason Hansen <jason_hansen@outlook.com>
2017-09-18 12:40:51 -0700

    [lore-hook-auth] fix tests

--- packages/lore-hook-auth/test/defaults.spec.js
+++ packages/lore-hook-auth/test/defaults.spec.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect;
 var definition = require('../src/index');
 var Hook = require('lore-utils').Hook;
+import get from '../src/actions/get';
+import update from '../src/actions/update';
 
 describe('defaults', function() {
 
@@ -8,6 +10,10 @@ describe('defaults', function() {
     var hook = new Hook(definition);
     var defaultConfig = {
       auth: {
+        blueprints: {
+          get,
+          update
+        },
         modelName: null,
         actionName: null,
         reducerName:null
