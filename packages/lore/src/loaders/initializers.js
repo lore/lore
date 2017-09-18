f388c255e0a88366f240b62d3b8419cd1b6237b0
Jason Hansen <jason_hansen@outlook.com>
2017-09-18 14:02:13 -0700

    [lore] configure initializers loader to work with ES6 modules

--- packages/lore/src/loaders/initializers.js
+++ packages/lore/src/loaders/initializers.js
@@ -5,7 +5,8 @@ export default {
   load: function() {
     const req = require.context(`${__LORE_ROOT__}/initializers`, true, /\.js$/);
     return req.keys().map(function(key) {
-      return req(key);
+      const initializer = req(key);
+      return initializer.__esModule ? initializer.default : initializer;
     });
   }
 
