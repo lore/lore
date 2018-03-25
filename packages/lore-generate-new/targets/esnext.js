b2f681101d2811defdbc2696b0a4076d2a8027ff
Jason Hansen <jason_hansen@outlook.com>
2018-03-25 14:27:35 -0700

    [lore-generate-new] remove all README files

--- packages/lore-generate-new/targets/esnext.js
+++ packages/lore-generate-new/targets/esnext.js
@@ -28,29 +28,23 @@ module.exports = {
   './routes.js': {copy: es6('routes.js')},
   './webpack.config.js': {copy: es6('webpack.config.js')},
 
-  // src
-  './src/README.md': {copy: common('src/README.md')},
-
   // actions
-  './src/actions/README.md': {copy: common('src/actions/README.md')},
+  './src/actions/.gitkeep': {copy: common('.gitkeep')},
 
   // collections
-  './src/collections/README.md': {copy: common('src/collections/README.md')},
+  './src/collections/.gitkeep': {copy: common('.gitkeep')},
 
   // components
-  './src/components/README.md': {copy: common('src/components/README.md')},
   './src/components/Layout.js': {copy: es6('src/components/Layout.js')},
   './src/components/Master.js': {copy: esnext('src/components/Master.js')},
   './src/components/RemoveLoadingScreen.js': {copy: es6('src/components/RemoveLoadingScreen.js')},
   './src/components/ShowLoadingScreen.js': {copy: es6('src/components/ShowLoadingScreen.js')},
 
   // constants
-  './src/constants/README.md': {copy: common('src/constants/README.md')},
   './src/constants/ActionTypes.js': {copy: es6('src/constants/ActionTypes.js')},
   './src/constants/PayloadStates.js': {copy: es6('src/constants/PayloadStates.js')},
 
   // decorators
-  './src/decorators/README.md': {copy: common('src/decorators/README.md')},
   './src/decorators/UserIsAuthenticated.js': {copy: es6('src/decorators/UserIsAuthenticated.js')},
   './src/decorators/UserIsAuthorized.js': {copy: es6('src/decorators/UserIsAuthorized.js')},
 
@@ -61,14 +55,12 @@ module.exports = {
   './src/forms/.gitkeep': {copy: common('.gitkeep')},
 
   // models
-  './src/models/README.md': {copy: common('src/models/README.md')},
   './src/models/currentUser.js': {copy: es6('src/models/currentUser.js')},
 
   // reducers
-  './src/reducers/README.md': {copy: common('src/reducers/README.md')},
+  './src/reducers/.gitkeep': {copy: common('.gitkeep')},
 
   // utils
-  './src/utils/README.md': {copy: common('src/utils/README.md')},
   './src/utils/auth.js': {copy: es6('src/utils/auth.js')},
   './src/utils/storageAvailable.js': {copy: es6('src/utils/storageAvailable.js')},
 
@@ -85,18 +77,15 @@ module.exports = {
   './config/reducers.js': {copy: es6('config/reducers.js')},
   './config/redux.js': {copy: es6('config/redux.js')},
   './config/router.js': {copy: es6('config/router.js')},
-  './config/README.md': {copy: common('config/README.md')},
 
   // config/env
   './config/env/development.js': {copy: es6('config/env/development.js')},
   './config/env/production.js': {copy: es6('config/env/production.js')},
-  './config/env/README.md': {copy: common('config/env/README.md')},
 
   // initializers
-  './initializers/REAMDE.md': {copy: common('initializers/README.md')},
+  './initializers/.gitkeep': {copy: common('.gitkeep')},
 
   // assets
-  './assets/REAMDE.md': {copy: common('assets/README.md')},
   './assets/css/loading-screen.css': {copy: common('assets/css/loading-screen.css')},
   './assets/css/main.css': {copy: common('assets/css/main.css')},
   './assets/images/favicon.png': {copy: common('assets/images/favicon.png')},
