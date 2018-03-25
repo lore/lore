e9401c0ead488eeef003f94b48711b73e0161ec8
Jason Hansen <jason_hansen@outlook.com>
2018-03-25 14:26:50 -0700

    [lore-generate-new] improve messaging in auth utility

--- packages/lore-generate-new/templates/es6/src/utils/auth.js
+++ packages/lore-generate-new/templates/es6/src/utils/auth.js
@@ -6,10 +6,10 @@
 import storageAvailable from './storageAvailable';
 
 // If the user's browser doesn't support localStorage, we can use this variable
-// as a fallback to store the their authentication token. This will allow them
-// to use use the application, but if they refresh the browser (or navigate away
-// from the application, this variable will be reset, and they'll be logged
-// out of the application since we've lost the token that provided their identity).
+// as a fallback to store their authentication token. This will allow them to use
+// the application, but if they refresh the browser (or navigate away from the
+// application), this variable will be reset, and they'll be logged out of the
+// application since we've lost the token that provided their identity.
 let tokenFallback = null;
 
 export default {
@@ -20,9 +20,9 @@ export default {
     } else {
       tokenFallback = token;
       let message = `
-      Your web browser does not support storing settings locally. In Safari, the most common cause of this
-      is using "Private Browsing Mode". This will not affect the functionality of the application, but you
-      will be logged out if you refresh the page.
+      Your web browser does not allow access to local storage. In Safari, the most common cause of 
+      this is using "Private Browsing Mode". This will not affect the functionality of this application, 
+      but you will be logged out if you leave the site or refresh the page.
       `;
       message = message.split('\n').join(' ').trim();
       alert(message);
