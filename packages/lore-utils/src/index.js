76bda256bde01c0cf74d6bf21f7f83a8cc5c023b
Jason Hansen <jason_hansen@outlook.com>
2019-07-04 18:23:14 -0700

    [lore-utils] delete old Hook code

--- packages/lore-utils/src/index.js
+++ packages/lore-utils/src/index.js
@@ -1,7 +1,6 @@
 import buildDictionary from 'webpack-requiredir';
 import ActionTypes from './ActionTypes';
 import PayloadStates from './PayloadStates';
-import Hook from './Hook';
 import payload from './payload';
 import payloadCollection from './payloadCollection';
 import result from './result';
@@ -11,7 +10,6 @@ import getModuleFromContext from './getModuleFromContext';
 export {
   ActionTypes,
   PayloadStates,
-  Hook,
   payload,
   payloadCollection,
   result,
