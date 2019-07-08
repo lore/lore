9fce04369342935f8f26fec9239eba52a69af9bc
Jason Hansen <jason_hansen@outlook.com>
2019-07-07 20:57:16 -0700

    [lore-initializers] break up index.js

--- packages/lore-initializers/src/index.js
+++ packages/lore-initializers/src/index.js
@@ -1,27 +1,2 @@
-import _ from 'lodash';
-import buildDictionary from 'webpack-requiredir';
-
-/*
- * Import all files in /initializers and convert into a dictionary, where the key
- * is the name of the file
- */
-
-const modules = buildDictionary(require.context(`${__LORE_ROOT__}/initializers`, false, /\.js$/));
-
-/*
- * Get the initializers
- */
-
-export function getInitializers(config) {
-  return modules;
-}
-
-/*
- * Execute each initializer
- */
-
-export function runInitializers(config, { initializers }) {
-  return _.mapValues(initializers, function(initializer) {
-    initializer(config);
-  });
-}
+export { getInitializers } from './getInitializers';
+export { runInitializers } from './runInitializers';
