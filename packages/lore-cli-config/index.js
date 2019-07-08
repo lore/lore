842d99e2c0cb7149f5549e91f638fc8521a862d8
Jason Hansen <jason_hansen@outlook.com>
2019-07-08 15:25:30 -0700

    [lore-cli-config] remove error to allow cli to work outside a project

--- packages/lore-cli-config/index.js
+++ packages/lore-cli-config/index.js
@@ -3,11 +3,6 @@ var _ = require('lodash');
 
 module.exports = function getConfig(defaults = {}) {
   const path = process.cwd();
-  const result = cosmiconfig('lore').searchSync(path);
-
-  if (!result) {
-    throw new Error(`No Lore config found in: ${path}`)
-  }
-
+  const result = cosmiconfig('lore').searchSync(path) || {};
   return _.merge(defaults, result.config || {});
 };
